977e89f9a5551928abd106924d44c4988d488ca6
HyeRyeong <fud3725@naver.com>
2022-08-26 16:25:31 +0900

    [feat] aws Service 파일

---
+++ src/main/java/Togefit/server/service/AwsS3Service.java
@@ -0,0 +1,59 @@
+package Togefit.server.service;
+
+import Togefit.server.response.error.CustomException;
+import Togefit.server.response.error.Error;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class AwsS3Service {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+    private final AmazonS3Client amazonS3Client;
+
+    public AwsS3Service(AmazonS3Client amazonS3Client) {
+        this.amazonS3Client = amazonS3Client;
+    }
+
+    private boolean isFileExists(MultipartFile multipartFile){
+        if(multipartFile.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+
+
+        String fileName = buildFileName(multipartFile.getOriginalFilename());
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try(InputStream inputStream = multipartFile.getInputStream()){
+            amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        }catch(IOException e){
+            throw new CustomException(new Error("업로드에 실패하였습니다."));
+        }
+
+        return amazonS3Client.getUrl(bucketName, fileName).toString();
+    }
+
+    private String buildFileName(String originalFileName){
+        int fileExtensionIndex = originalFileName.lastIndexOf(".");
+        String fileExtension = originalFileName.substring(fileExtensionIndex);
+        String fileName = originalFileName.substring(0, fileExtensionIndex);
+        String now = String.valueOf(System.currentTimeMillis());
+
+        return fileName + "_" + now + fileExtension;
+    }
+}
